534d684e18adcb728e10808069b03054efa9ae62
Site web <robin.pougea@hotmail.fr>
2022-01-05 20:51:17 +0100

    Site web version 1 beta

---
+++ Site/retenir.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Video en lecture automatique > activité débosselage";
+echo "2-3 Typos";
+echo "Formulaire prestation"
+echo "Enregistrer mail/num clients > Excel";
+echo "Parler en Nom propre / Entreprise";
+echo "newsletter?";
+echo "créer un alias mail robin.pougea@debosselage.fr";
+echo "zone géographique d'activité";
+echo "charte graphique / Business plan / Etude de marché" ;
+echo "Suisse/France cadre présentation";
+echo "Ajouter commentaires sur le site";
+ ?>
